de53e9957f17991706be78dc449027de91c05a1d
Boris Sekachev <boris.sekachev@intel.com>
2018-10-18 14:50:10 +0300

    git_anno_repos app initialization

---
+++ cvat/apps/git_anno_repos/apps.py
@@ -0,0 +1,10 @@
+
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from django.apps import AppConfig
+
+
+class GitAnnoReposConfig(AppConfig):
+    name = 'git_anno_repos'
